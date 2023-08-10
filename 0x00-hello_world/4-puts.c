ad362117e796cdb76463e732bc2fc0ddcf501da1
slapjackke <mhgreat678@gmail.com>
2023-08-10 05:25:12 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main- the start
+ *Return: the value if success 
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
